36d9f3bf7b6ad8352d8b02a8be86576d631af246
Soheil-Rameznie <s.ramezani.official@gmail.com>
2024-11-20 16:31:35 +0330

    filmListPagination base added

--- src/views/HomePage/main/MovieList/HomeMovieList.vue
+++ src/views/HomePage/main/MovieList/HomeMovieList.vue
@@ -1,13 +1,28 @@
 <template>
-  
+  <div class="right-side d-inline-block">
+    <section class="site-posts d-block w-100">
+      <div ref="movies"></div>
+    </section>
+    <section class="site-posts d-block w-100">
+      <div ref="pagination" class="pagination w-100 d-flex flex-wrap justify-content-center"></div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-name:"HomeMovieList"
+  name: 'HomeMovieList',
+  data() {
+    return {
+        currentPage : 1,
+        rowsCount : 6,
+    }
+  },
+  methods(){
+ 
+  }
 }
 </script>
 
 <style>
-
 </style>
